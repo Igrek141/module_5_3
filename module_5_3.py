76b3d703048bc2044cf633162c871cfdf6ddc5db
Igrek141 <igor141@mail.ru>
2024-10-17 10:13:12 +0300

    Add files via upload

--- module_5_3.py
+++ module_5_3.py
@@ -18,32 +18,49 @@ class House:
         return self.number_of_floors
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
 
     def __add__(self, value):
-        self.number_of_floors += value
+        if isinstance(value, int):
+            self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
         return self
 
     def __iadd__(self, other):
-        return self.__add__(other)
+        if isinstance(other, int):
+            self.number_of_floors += other
+        elif isinstance(other, House):
+            self.number_of_floors += other.number_of_floors
+        return self
 
     def __radd__(self, other):
-        return self.__add__(other)
+        if isinstance(other, int):
+            self.number_of_floors += other
+        elif isinstance(other, House):
+            self.number_of_floors += other.number_of_floors
+        return self
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        return not self.__le__(other)
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        return not self.__lt__(other)
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        return self.__eq__(other) or self.__lt__(other)
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        return not self.__eq__(other)
 
 
 h1 = House('ЖК Эльбрус', 10)
